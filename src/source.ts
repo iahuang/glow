17c3a4ba1da4a13f680a639ac2f97d95e2746c28
Ian Huang <iahuang@users.noreply.github.com>
2021-05-11 19:12:47 -0400

    token matcher

---
+++ src/source.ts
@@ -0,0 +1,60 @@
+import fs from "fs";
+import path from "path";
+
+export class SourceFile {
+    path: string | null;
+    name: string;
+    contents: string;
+
+    // a cache of the source contents split by line
+    _lines: string[];
+
+    constructor(filePath: string) {
+        this.path = filePath;
+        this.name = path.basename(filePath);
+        let contents = fs.readFileSync(filePath, "utf-8");
+
+        // internally, the glow compiler uses unix line endings.
+        contents = contents.replace(/\r\n/g, "\n");
+        this.contents = contents;
+
+        this._lines = this.contents.split("\n");
+    }
+
+    getLineByNumber(lineNo: number) {
+        return this._lines[lineNo + 1];
+    }
+
+    getPosByCharIndex(index: number) {
+        // Returns a line:col pair from a string character index of the whole
+        // source file
+
+        let cumulativeCharIndex = 0;
+        for (let lineIndex = 0; lineIndex < this._lines.length; lineIndex++) {
+            // add +1 to account for the \n character
+            let lineLength = this._lines[lineIndex].length + 1;
+            cumulativeCharIndex += lineLength;
+
+            // check if we're on the correct line
+            // i.e. if the next line would go past the provided index
+            if (cumulativeCharIndex > index) {
+                let lineContent = this._lines[lineIndex];
+
+                // to find the column of the index, take the index of the start of this line
+                let indexAtStart = cumulativeCharIndex - lineLength;
+                // ...and subtract it from the target index, adding one (column no. starts at one, not zero)
+                let col = index - indexAtStart + 1;
+                return {
+                    line: lineIndex + 1,
+                    col: col,
+                };
+            }
+        }
+        throw new Error(`Cannot get source position ${index} out of range ${this.contents.length}`);
+    }
+}
+
+interface SourcePos {
+    col: number;
+    line: number;
+}
--- src/tokenizer/token.ts
+++ src/tokenizer/token.ts
@@ -1,14 +1,25 @@
+import { SourceFile } from "../source";
+
 export interface Token {
     type: TokenType;
     content: string;
-    source: string;
+    source: SourceFile;
     line: number;
     col: number;
 }
 
 export enum TokenType {
+    // A symbol such as a variable or function name.
     Name,
+
+    // String character token; represents any character inside a string literal.
     StringChar,
+
+    // Special tokens
+    EOL, // represents "\n" or "\r\n"
+    EOF, // represnets the end of the source file.
+
+    // Basic syntax tokens
     DblQuote,
     CurlyBraceRight,
     CurlyBraceLeft,
@@ -20,13 +31,107 @@ export enum TokenType {
     DoubleEq,
     Colon,
     Semicolon,
-    EOF,
-    EOL,
     Astrisk,
     Plus,
     Minus,
     Div,
+
+    // Keywords
     KWVar,
     KWConst,
-    KWFunc
+    KWFunc,
+}
+
+interface TokenPattern {
+    regex?: RegExp;
+    stringMatch?: string;
+    tokenType: TokenType;
+}
+
+/*
+    The TokenMatcher class contains definitions for many of the above syntax tokens.
+
+    the method TokenMatcher.match() returns a list of all the token types + matched strings that fall at the start
+    of the provided string.
+*/
+
+export class TokenMatcher {
+    tokenPatterns = new Map<TokenType, TokenPattern>();
+
+    constructor() {
+        this.addRegexPattern(TokenType.Name, /[a-zA-Z_][a-zA-Z_0-9]{0,}/)
+            .addRegexPattern(TokenType.StringChar, /./)
+            .addRegexPattern(TokenType.EOF, /$/)
+            .addRegexPattern(TokenType.EOL, /\n/)
+            .addPattern(TokenType.DblQuote, '"')
+            .addPattern(TokenType.CurlyBraceLeft, "{")
+            .addPattern(TokenType.CurlyBraceRight, "}")
+            .addPattern(TokenType.SquareBrackLeft, "[")
+            .addPattern(TokenType.SquareBrackRight, "]")
+            .addPattern(TokenType.ParenLeft, "(")
+            .addPattern(TokenType.ParenRight, ")")
+            .addPattern(TokenType.Assign, "=")
+            .addPattern(TokenType.DoubleEq, "==")
+            .addPattern(TokenType.Colon, ":")
+            .addPattern(TokenType.Semicolon, ";")
+            .addPattern(TokenType.Astrisk, "*")
+            .addPattern(TokenType.Plus, "+")
+            .addPattern(TokenType.Minus, "-")
+            .addPattern(TokenType.Div, "/")
+            .addPattern(TokenType.KWConst, "const")
+            .addPattern(TokenType.KWVar, "var")
+            .addPattern(TokenType.KWFunc, "func");
+    }
+
+    match(source: string, allowedTokenType: TokenType[]) {
+        let matches = [];
+
+        for (let tokenType of allowedTokenType) {
+            let tokenPattern = this.tokenPatterns.get(tokenType);
+            if (!tokenPattern) throw new Error(`Parsing method for token type ${TokenType[tokenType]} is not defined!`);
+            let match = this._matchToken(source, tokenPattern);
+            if (match) {
+                matches.push({
+                    tokenType: tokenType,
+                    matchedString: match,
+                });
+            }
+        }
+
+        return matches;
+    }
+
+    _matchToken(source: string, pattern: TokenPattern): string | null {
+        /*
+            if the source string () matches the provided token pattern
+            return the matching token contents.
+            otherwise, return null.
+        */
+
+        if (pattern.regex) {
+            let regexMatch = pattern.regex.exec(source);
+            if (!regexMatch) return null;
+
+            // the regex pattern should really only match the beginning of the string; just make sure
+            if (!source.startsWith(regexMatch[0])) return null;
+
+            return regexMatch[0];
+        }
+        if (pattern.stringMatch) {
+            // otherwise, if searching for an exact string
+            // just see if the source string starts with the token
+            if (source.startsWith(pattern.stringMatch)) return pattern.stringMatch;
+        }
+        return null;
+    }
+
+    addPattern(forToken: TokenType, pattern: string) {
+        this.tokenPatterns.set(forToken, { tokenType: forToken, stringMatch: pattern });
+        return this;
+    }
+
+    addRegexPattern(forToken: TokenType, regex: RegExp) {
+        this.tokenPatterns.set(forToken, { tokenType: forToken, regex: regex });
+        return this;
+    }
 }
